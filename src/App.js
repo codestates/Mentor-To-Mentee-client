ca52ea1782179db7755a9abf63fd499267b8e354
강희석 <44192757+dktkwhdk1@users.noreply.github.com>
2020-12-24 22:04:44 +0900

    Fix error

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
+import './App.css';
+import Header from './component/Header';
+import Main from './component/Main';
+import Footer from './component/Footer';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+/*
 import React from 'react';
 import './App.css';
 import MyPage from './components/MyPage';
@@ -7,3 +25,4 @@ function App() {
 }
 
 export default App;
+*/
